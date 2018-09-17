8f7d30a105e4e9f6c82a1c67eb947277752af530
3RebeccaSila3 <silarebecca26@gmail.com>
2018-09-18 02:51:19 +0300

    updated 3 files with template and class logics

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  title = 'Quotes'; 
 
+  quotes = [
+    new Quote(1, 'Life is way too short for bad vibes'),
+    new Quote(2, 'There is a voice that does not use words,Listen',),
+    new Quote(3, 'A day without a laugh is a wasted day',),
+    new Quote(4, 'If it does not challenge you,it does not change you',),
+    new Quote(5, 'Friends are like rainbows always there to cheer you up after the storm'),
+ ]
   constructor() { }
 
   ngOnInit() {
