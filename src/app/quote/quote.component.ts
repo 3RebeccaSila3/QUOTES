9755a8206b7c291972319759c51298fd902aecce
3RebeccaSila3 <silarebecca26@gmail.com>
2018-09-18 09:50:44 +0300

    add a date pipe and chained it to extend the transformation

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,11 +11,11 @@ export class QuoteComponent implements OnInit {
   color = 'yellow'; 
 
   quotes = [
-    new Quote(1, 'Life is way too short for bad vibes', 'Anonymous Rebecca Sila',),
-    new Quote(2, 'There is a voice that does not use words,Listen', 'Rumi Rebecca Sila', ),
-    new Quote(3, 'A day without a laugh is a wasted day', 'Charles Chaplin Janet Isak',),
-    new Quote(4, 'If it does not challenge you,it does not change you', 'Adewaje Frank Robert',),
-    new Quote(5, 'Friends are like rainbows always there to cheer you up after the storm', 'Charles Chaplin Jade',),
+    new Quote(1, 'Life is way too short for bad vibes', 'Anonymous Rebecca Sila',new Date(2018,8,16)),
+    new Quote(2, 'There is a voice that does not use words,Listen', 'Rumi Rebecca Sila',new Date(2018,8,12) ),
+    new Quote(3, 'A day without a laugh is a wasted day', 'Charles Chaplin Janet Isak', new Date(2018,8,11)),
+    new Quote(4, 'If it does not challenge you,it does not change you', 'Adewaje Frank Robert', new Date(2018,8,10)),
+    new Quote(5, 'Friends are like rainbows always there to cheer you up after the storm', 'Charles Chaplin Jade', new Date(2018,8,6)),
  ]
 
  completeQuote(isComplete,index){
